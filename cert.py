6a2b8a6570659fde664e3f4890cd8f86db487e0f
geek9012 <84989098+geek9012@users.noreply.github.com>
2021-07-29 20:56:57 +0530

    Update cert.py

--- cert.py
+++ cert.py
@@ -4,12 +4,7 @@ import cv2 as cv
 import os
 import base64
 
-def get_binary_file_downloader_html(bin_file, file_label='File'):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
-    return href
+
     
 st.header('Customize your COVID certificate')    
 
